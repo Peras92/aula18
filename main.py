59229e2e6734ec31ebe5182c1463adc918fc7de8
Rui Pereira <ruisilvapereira1@gmail.com>
2021-05-21 11:28:11 +0100

    teste de paginação

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sqla_wrapper import SQLAlchemy
 import os
+from sqlalchemy_pagination import paginate
 
 app = Flask(__name__)
 
@@ -18,7 +19,16 @@ db.create_all()
 
 @app.route("/")
 def index():
-    mensagem = db.query(Mensagem).all()
+
+    page = request.args.get("page")
+
+    if not page:
+        page=1
+
+    mensagem_filtrada = db.query(Mensagem)
+
+    mensagem = paginate(query=mensagem_filtrada, page=int(page), page_sige=5)
+
     return render_template("index.html", mensagem=mensagem)
 
 @app.route("/add-message", methods=["POST"])
